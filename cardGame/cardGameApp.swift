300cd92ea66a93b01fa14f9d4ec2f074d1edf67a
Colin Myers <108821534+colinmyers37@users.noreply.github.com>
2023-10-13 22:09:03 -0500

    Initial Commit

---
+++ cardGame/cardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardGameApp.swift
+//  cardGame
+//
+//  Created by Colin Myers on 10/13/23.
+//
+
+import SwiftUI
+
+@main
+struct cardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
